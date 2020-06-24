a90d8e28cddab537c87971bcef23d9c41a58f04d
trungtran1211 <56382373+trungtran1211@users.noreply.github.com>
2020-06-24 14:34:17 +0700

    trung

--- Helloworld/app/src/main/java/com/example/helloworld/LoginActivity.java
+++ Helloworld/app/src/main/java/com/example/helloworld/LoginActivity.java
@@ -4,23 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 public class LoginActivity extends AppCompatActivity {
-    private Button btnLogin;
+    EditText edtuser,edtpassword;
+    Button btndangky,btndangnhap;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btnLogin = (Button) findViewById(R.id.btn_login);
+        Anhxa();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btndangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                String username = "admin";
+                String password = "admin";
+
+                if (edtuser.getText().toString().equals(username) && edtpassword.getText().toString().equals(password)){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Đăng nhập thất bại", Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    private void Anhxa() {
+        edtuser = (EditText)findViewById(R.id.edittextuser);
+        edtpassword = (EditText)findViewById(R.id.edittextpassword);
+        btndangky = (Button)findViewById(R.id.buttondangky);
+        btndangnhap = (Button)findViewById(R.id.buttondangnhap);
     }
 }
